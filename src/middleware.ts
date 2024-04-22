3f369c28dab2d57f00eb407f1143e4f824afaccb
Jonathan Thuayre <jthuayre@wizards-technologies.com>
2024-04-22 14:59:21 +0200

    fix : new middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,47 @@
-import { i18n } from '@/i18n-config'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
+import { i18n } from "./i18n-config";
+import { MiddlewareRequest, NextRequest } from "@netlify/next";
 
-export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname
+const defaultLocale = i18n.defaultLocale;
+
+export async function middleware(req: NextRequest) {
+  let pathname = req.nextUrl.pathname;
+  const searchParams = req.nextUrl.search;
+  const locale = pathname.split("/")[1];
+
+  const middlewareRequest = new MiddlewareRequest(req);
 
   // Check if the default locale is in the pathname
-  if (
-    pathname.startsWith(`/${i18n.defaultLocale}/`) ||
-    pathname === `/${i18n.defaultLocale}`
-  ) {
-    // e.g. incoming request is /en/about
-    // The new URL is now /about
-    return NextResponse.redirect(
-      new URL(
-        pathname.replace(
-          `/${i18n.defaultLocale}`,
-          pathname === `/${i18n.defaultLocale}` ? '/' : ''
-        ),
-        request.url
-      )
-    )
+  if (defaultLocale === locale) {
+    console.log("remove default locale");
+    // we remove locale if pathname contains default locale, by redirecting
+    if (pathname.startsWith(`/${locale}/`)) {
+      // if pathname has subpath, remove "/locale", eg. from "/ca/something" to "/something"
+      return NextResponse.redirect(new URL(`${pathname.replace(`/${locale}`, "")}${searchParams}`, req.url));
+    } else {
+      // if it doesn't, remove "locale", eg. from "/ca to "/"
+      return NextResponse.redirect(new URL(`${pathname.replace(`${locale}`, "")}${searchParams}`, req.url));
+    }
   }
 
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+  const pathnameIsMissingValidLocale = i18n.locales.every((locale) => {
+    return !pathname.startsWith(`/${locale}`);
+  });
 
-  if (pathnameIsMissingLocale) {
-    // We are on the default locale
-    // Rewrite so Next.js understands
-
-    // e.g. incoming request is /about
-    // Tell Next.js it should pretend it's /en/about
-    return NextResponse.rewrite(
-      new URL(`/${i18n.defaultLocale}${pathname}`, request.url)
-    )
+  if (pathnameIsMissingValidLocale) {
+    // Remove trailing slash
+    if (pathname === "/") pathname = "";
+    // we rewrite pathnames without valid locale: visit `/ca`, but browser url shows `/`
+    // Incoming request: "/hello"
+    // Rewritten request: page shown "/en/hello", browser url "/hello"
+    return middlewareRequest.rewrite(
+      // Note: Pathname already includes "/"
+      new URL(`/${defaultLocale}${pathname}${searchParams}`, req.url)
+    );
   }
 }
 
 export const config = {
-  // matcher solution for public, api, assets and _next exclusion
-  // matcher: '/((?!api|static|.*\\..*|_next).*)',
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
@@ -49,6 +49,6 @@ export const config = {
      * - _next/static (static files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|admin|_next/static|_next/image|_ipx|assets|favicon.ico|public).*)",
+    "/((?!api|admin|_next/static|_next/image|_ipx|assets|favicon.ico|under-development.svg|public).*)",
   ],
-}
+};
